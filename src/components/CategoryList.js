9e00d6d33d2fd4cdbe18682161607e29ab88042d
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-01-19 23:51:31 +0100

    event

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,13 +1,18 @@
-import React, { Component } from "react";
+import React, { Component,useState } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
   state = {
     categories: [
       { arabaId: 1, arabaModel: "mercedes" },
-      { arabaId: 2, arabaModel: "mercedes" },
-      { arabaId: 3, arabaModel: "mercedes" },
+      { arabaId: 2, arabaModel: "audo" },
+      { arabaId: 3, arabaModel: "skida" },
     ],
+    arabaCategory7:"9"
+  };
+
+  changeCategory = category => {
+    this.setState({ arabaCategory7:category.arabaModel });
   };
 
   render() {
@@ -16,11 +21,15 @@ export default class CategoryList extends Component {
         <h2>{this.props.title}</h2>
         <ListGroup flush>
           {this.state.categories.map((category) => (
-            <ListGroupItem key={category.arabaId}>
-              {category.arabaModel}{" "}
+            <ListGroupItem
+              onClick={() => this.changeCategory(category)}
+              key={category.arabaId}
+            >
+              {category.arabaModel}
             </ListGroupItem>
           ))}
         </ListGroup>
+        <h4>{this.state.arabaCategory7}r</h4>
       </div>
     );
   }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CategoryList from './CategoryList';
+
 
 function ProductList(props) {
 
